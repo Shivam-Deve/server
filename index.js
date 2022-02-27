b3c8705891623a46469da38f171923a8c9037891
Shivam <smshivammishra1998@gmail.com>
2022-02-27 09:27:01 +0530

    express routing

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.listen(5000);
+app.get('/', (req, res) => {
+    res.sendFile('./Views/index.html', { root: __dirname })
+})
+app.get('/about', (req, res) => {
+    res.sendFile('./Views/about.html', { root: __dirname })
+})
+app.get('/about-us', (req, res) => {
+    res.redirect('/about')
+})
+app.use((req, res) => {
+    res.sendFile('./Views/404.html', { root: __dirname })
+})
